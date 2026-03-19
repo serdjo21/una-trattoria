a2d872218a82fb6d493d8c4efb157c97d5f6bd52
serdjo21 <serdjo21@icloud.com>
2026-03-19 22:36:37 +0100

    redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'host',
+            value: 'unatrattoria.rs'
+          }
+        ],
+        destination: 'https://www.unatrattoria.rs/:1',
+        permanent: true, 
+      },
+    ]
+  }
 };
 
 
