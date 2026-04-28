13d598fbcf4a21511335295f32c6c7611d1ccf97
serdjo21 <serdjo21@icloud.com>
2026-04-28 19:15:55 +0200

    en

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,7 +74,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="sr" className={`${inter.variable} ${playfair.variable}`}>
+    <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <head>
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
